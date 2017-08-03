6217d7cd231c850ddfbb19270b76f963cadb4943
K. M. Lawson <regs@huginn.net>
2017-08-03 22:00:41 +0100

    added lines for neovim

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -29,3 +29,6 @@ for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
+
+mkdir -p ~/.config/nvim
+ln -s $dir/init.vim ~/.config/nvim/init.vim
